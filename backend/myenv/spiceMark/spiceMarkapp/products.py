36d74223098db30f4ecf13fec2f7c89650cba0fc
Kuria-FelixM <muhiafelix35@gmail.com>
2025-01-17 07:38:01 +0300

    b-views

---
+++ backend/myenv/spiceMark/spiceMarkapp/products.py
@@ -0,0 +1,20 @@
+products = [
+    {
+        'id': 1,
+        'name': 'SpiceMark',
+        'category': 'Spices',
+        'price': 10
+    },
+    {
+        'id': 2,
+        'name': 'Nike',
+        'category': 'Shoes',
+        'price': 100
+    },
+    {
+        'id': 3,
+        'name': 'Adidas',
+        'category': 'Shoes',
+        'price': 100
+    }
+]
--- backend/myenv/spiceMark/spiceMarkapp/urls.py
+++ backend/myenv/spiceMark/spiceMarkapp/urls.py
@@ -2,5 +2,6 @@ from spiceMarkapp import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.getRoutes, name='getRoutes'),
+    path('routes/', views.getRoutes, name='getRoutes'),
+    path('products/', views.getProducts, name='getProducts')
 ]
