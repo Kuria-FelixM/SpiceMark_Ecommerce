8f75c4736709ff6373502b4d6ab0a704b6b72c11
Kuria-FelixM <muhiafelix35@gmail.com>
2025-03-20 09:55:39 -0400

    frontend-setup

--- README.md
+++
@@ -1,1 +0,0 @@
-# SpiceMark_Ecommerce
---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Container></Container>
+      <Footer />
+
+    </div>
+  )
+}
+
+export default App
