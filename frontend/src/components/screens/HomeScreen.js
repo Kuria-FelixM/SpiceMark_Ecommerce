e804c9c5e0a5304846d576e6e8607412328f4164
Kuria-FelixM <muhiafelix35@gmail.com>
2025-03-24 10:31:04 -0400

    models

--- frontend/src/components/screens/HomeScreen.js
+++ frontend/src/components/screens/HomeScreen.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useEffect,useState} from 'react'
+import { Container } from 'react-bootstrap'
+import axios from 'axios'
+import { Row,Col } from 'react-bootstrap'
 
 function HomeScreen() {
+
+  const [products,setProducts]=useState([])
+
+  useEffect(()=>{
+    async function fetchproducts(){
+      const {data} = await axios.get('/api/products/')
+      setProducts(data)
+    }
+    fetchproducts()
+  },[])
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <h1>Products</h1>
+      <Row>
+        {products.map((product)=>(
+          <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
+            <h3>{product.productName}</h3>
+            <h6>{product.price}</h6>
+          </Col>
+          
+        ))}
+      </Row>
+    </Container>
   )
 }
 
